717175c3b9106cd38ab63ccde68aecdff3eb415c
Anastasiia <anastacia.vashuck@gmail.com>
2020-11-01 23:10:58 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+from tkinter import *
+
+from controller import *
+from battleField import *
+from startBoard import *
+from battleBoard import *
+
+
+root = Tk()
+app = StartBoard(master=root)
+app.mainloop()
+
